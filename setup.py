d07442f728a39d928c116aec28819a6215c45c71
msouza-quant <138340846+msouza2023@users.noreply.github.com>
2023-07-06 13:12:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cowsol",
-    version='0.1',
+    version='0.2',
     packages=find_packages(include=['src', \
                     'src.apis', \
                     'src.strategies', \
